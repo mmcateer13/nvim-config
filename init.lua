8b7e33959b14ea6b8753425390160ef31bc8932f
Matthew McAteer <MatthewMcA2001@gmail.com>
2025-09-19 23:23:59 +0100

    Move myconfig module imports into init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,8 @@
-require("myconfig.options")
-require("myconfig.keymaps")
-
+require("myconfig")
 require("plugins")
 
 -- TODO: Lualine
 -- TODO: Format on save/autocmd
 -- TODO: Maybe add a module/package for divvy-specific stuff?
 -- TODO: A floating window/scratch buffer for listing all keymaps with the leader key
+-- TODO: A stylua hook
---
+++ lua/myconfig/init.lua
@@ -0,0 +1,4 @@
+-- Imports are usually ordered alphabetically, but not here.
+-- This order is needed so that keymaps are set with the correct leader key.
+require("myconfig.options")
+require("myconfig.keymaps")
