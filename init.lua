d5492b85a1a59a67d9c7f8540867b0b92e54023b
Matthew McAteer <MatthewMcA2001@gmail.com>
2025-11-12 16:16:34 +0000

    Remove TODOs from top-level init.lua
    I've migrated these TODOs to the GitHub repo's issues list.

--- init.lua
+++ init.lua
@@ -1,8 +1,2 @@
 require("myconfig").setup()
 require("plugins")
-
--- TODO: Lualine
--- TODO: Format on save/autocmd
--- TODO: Maybe add a module/package for divvy-specific stuff?
--- TODO: A floating window/scratch buffer for listing all keymaps with the leader key
--- TODO: A stylua hook
