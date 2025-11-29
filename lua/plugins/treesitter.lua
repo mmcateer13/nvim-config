956b40c4b94914bd66eac0a0430759fa5588f95b
Matthew McAteer <MatthewMcA2001@gmail.com>
2025-11-29 21:25:17 +0000

    Add Terraform language support
    As well as adding a Treesitter parser and an LSP for Terraform, this
    commit also tweaks the calls to setup for nvim-treesitter and
    mason-lspconfig so that they use the ensure_installed options. This
    not only means automatically keeping required parsers/LSPs synced across
    environments, but also means there is no need to manually write a script
    to do so.
    
    As a result, this commit resolves #11 and also resolves #13.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,31 @@
 local M = {}
 
 function M.setup()
-	require("nvim-treesitter.configs").setup({ highlight = { enable = true } })
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = {
+			"bash",
+			"json",
+			"lua",
+			"markdown",
+			"python",
+			"terraform",
+			"toml",
+			"yaml",
+		},
+
+		-- A note to self for some other options here where the documentation feels
+		-- a little unclear:
+		--
+		-- sync_install: whether parser installs are synchronous, i.e. blocking.
+		-- Enabling means full nvim startup will be blocked until installs are complete.
+		--
+		-- auto_install: whether missing parsers get pulled automatically. Fires
+		-- when opening a buffer to edit a new filetype.
+		sync_install = false,
+		auto_install = false,
+
+		highlight = { enable = true },
+	})
 end
 
 return M
